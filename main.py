252e11018a191efb78ad649ed7b7673e77ccefea
Eeeeeeeefg <byllooo682@gmail.com>
2024-11-16 19:48:18 -0500

    Completed rough Crawler class
    Crawler can add links to a dict, scrape the html in a loop, and convert it to text.
    Main creates an instance of Crawler, downloads GIRM and Sca. Con., and saves them to local files

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
-f = "GIRM.txt"
-r = requests.get("https://www.vatican.va/roman_curia/congregations/ccdds/documents/rc_con_ccdds_doc_20030317_ordinamento-messale_en.html")
-with open(f, "w", encoding='utf-8') as nF:
-    soup = BeautifulSoup(r.text, features="html.parser")
-    nF.write(soup.get_text())
+from webcrawler import Crawler
+
+c = Crawler()
+
+c.link_add("GIRM", "https://www.vatican.va/roman_curia/congregations/ccdds/documents/rc_con_ccdds_doc_20030317_ordinamento-messale_en.html")
+c.link_add("Sacrosanctum Concillium", "https://www.vatican.va/archive/hist_councils/ii_vatican_council/documents/vat-ii_const_19631204_sacrosanctum-concilium_en.html")
+c.link_add()
+c.scrape()
+output = c.soupify()
+for i in output.items():
+    with open(i[0], "w", encoding='utf-8') as f:
+        f.write(i[1].get_text())
--- webcrawler.py
+++ webcrawler.py
@@ -7,18 +7,18 @@ class Crawler:
     def __init__(self):
         self.encoding = 'utf-8'
         self.links = {}
-        self.scrape_results = []
+        self.scrape_results = {}
 
     def link_add(self, link_name: str, link: str):
         self.links.update({link_name: link})
 
     def scrape(self):
-        for i in self.links.values():
-            r = requests.get(i)
-            self.scrape_results.append(r.text)
+        for i in self.links.items():
+            r = requests.get(i[1])
+            self.scrape_results.update({i[0]: r.text})
 
     def soupify(self):
-        soup = []
-        for i in self.scrape_results:
-            soup.append(bs4.BeautifulSoup.get_text(i))
+        soup = {}
+        for i in self.scrape_results.items():
+            soup.update({i[0]: bs4.BeautifulSoup(i[1], features='html.parser')})
         return soup
